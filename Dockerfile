5f1e8fc680a9be610f2b3cfba120412612b22967
mfizia <m.fizia@gmx.de>
2020-11-19 09:47:19 +0100

    update to base image and go

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM ubuntu:16.04
+FROM ubuntu:20.04
 
 ARG COMMIT
 ENV COMMIT ${COMMIT:-master}
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update && apt-get install -y \
+RUN apt update && apt install -y \
     autoconf automake build-essential curl git libsnappy-dev libtool pkg-config
-
 RUN git clone https://github.com/openvenues/libpostal -b $COMMIT
 
 COPY ./*.sh /libpostal/
@@ -15,6 +14,11 @@ WORKDIR /libpostal
 RUN ./build_libpostal.sh
 RUN ./build_libpostal_rest.sh
 
+#CLEANUP
+RUN apt -y clean
+
 EXPOSE 8080
+
+USER nobody
 
 CMD /libpostal/workspace/bin/libpostal-rest
--- build_libpostal_rest.sh
+++ build_libpostal_rest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-curl https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz > go1.8.1.linux-amd64.tar.gz
+curl https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz > go1.15.5.linux-amd64.tar.gz
 
-echo "a579ab19d5237e263254f1eac5352efcf1d70b9dacadb6d6bb12b0911ede8994  go1.8.1.linux-amd64.tar.gz" | sha256sum -c
+echo "9a58494e8da722c3aef248c9227b0e9c528c7318309827780f16220998180a0d go1.15.5.linux-amd64.tar.gz" | sha256sum -c
 
-tar xzf go1.8.1.linux-amd64.tar.gz
+tar xzf go1.15.5.linux-amd64.tar.gz
 
 export GOROOT=/libpostal/go
 export GOPATH=/libpostal/workspace
